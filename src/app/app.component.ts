b2e7dbe8c9efb7988b4d2761b5c7394586c957c2
Tedy Darmawan <tedi.darmawan@mayora.co.id>
2018-08-16 10:27:08 +0700

    add wijmo data and control to the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+// import wijmo component
+import { CollectionView } from 'wijmo/wijmo';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Wijmo Starter App';
+  data = this.getData();
+
+  getData() {
+    var countries = 'US,Germany,UK,Japan,Italy,Greece'.split(','),
+        data = [];
+    for(var i=0; i<countries.length; i++) {
+      data.push({
+        country: countries[i],
+        sales: Math.random() * 10000,
+        expenses: Math.random() * 5000,
+        downloads: Math.round(Math.random() * 20000)
+      });
+    }
+
+    return new CollectionView(data);
+  }
 }
