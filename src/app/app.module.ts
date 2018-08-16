c655213935a6334cc63741ea02b0ea7a5d12f6cc
Tedy Darmawan <tedi.darmawan@mayora.co.id>
2018-08-16 10:25:40 +0700

    add wijmo modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// import wijmo modules
+import { WjGridModule } from 'wijmo/wijmo.angular2.grid';
+import { WjChartModule } from 'wijmo/wijmo.angular2.chart';
+
+// apply wijmo license key
+import { setLicenseKey } from 'wijmo/wijmo';
+setLicenseKey('your license key goes here');
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WjGridModule,
+    WjChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
